57488db8aaf5205fbe87000222282dce3c626059
ruman-dev <ruman.uits@gmail.com>
2023-06-11 20:00:07 +0600

    first commit

---
+++ app/src/main/java/com/rumanweb/banglanewsapp/MainActivity.java
@@ -0,0 +1,58 @@
+ package com.rumanweb.banglanewsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+ public class MainActivity extends AppCompatActivity {
+
+     ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.ListView);
+
+
+
+
+
+        MyAdapter myAdapter = new MyAdapter();
+        listView.setAdapter(myAdapter);
+
+
+
+    }
+
+
+    private class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+}
